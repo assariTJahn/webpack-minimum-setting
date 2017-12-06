57f532f512558e6f6e6e2f3ae905983d000dd803
Taejoon Ahn <taejoon.ahn@cfind.co.kr>
2017-12-06 18:02:06 +0900

    updated curtain interaction section

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import printMe from './print.js';
 import curtainStyle from './curtain/curtain.scss'
 import curtainHtml from './curtain/curtain.html'
 import curtainJs from './curtain/curtain.js'
 
-import { cube } from './math';
 
 function component() {
-    var element = document.createElement('pre');
-
-    // element.innerHTML = ['Hello webpack', '5cubed is equal to ' + cube(5)].join('\n\n');
-element.innerHTML = curtainHtml;
+    var element = document.createElement('div');
+    element.innerHTML = curtainHtml;
     
     return element;
 }
