97cac18d8871b0671de1265699dae8d5ad204060
Taejoon Ahn <taejoon.ahn@cfind.co.kr>
2017-12-07 16:31:06 +0900

    added entry point for eace component

---
+++ src/curtain/index.js
@@ -0,0 +1,8 @@
+import curtainStyle from './curtain.scss'
+import curtainHtml from './curtain.html'
+import curtainJs from './curtain.js'
+
+export default {
+    id: 'curtain',
+    content: curtainHtml
+};
---
+++ src/rotate/index.js
@@ -0,0 +1,8 @@
+import rotateHtml from './rotate.html';
+import rotateJs from './rotate.js';
+import rotateCss from './rotate.scss';
+
+export default {
+    id: 'rotate',
+    content: rotateHtml
+}
